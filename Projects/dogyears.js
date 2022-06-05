daf1718948b23c4528401dcb158a77c91fcd8404
sudipta-d4 <98043103+sudipta-d4@users.noreply.github.com>
2022-06-05 23:20:46 +0100

    dogyears.js

---
+++ Projects/dogyears.js
@@ -0,0 +1,17 @@
+// Variable of My Age
+const MyAge = 24
+
+// The Aging Process
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+// The first two years of my lived years
+let laterYears = myAge - 2;
+
+// My age in Dog Years
+laterYears *= 4;
+
+// Total Age of My Dog Years
+let myAgeInDogYears = earlyYears + laterYears;
+let myName = 'Sudipta'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
